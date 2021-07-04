98052820ab117f76c954999c374ccfd86c818b7d
Spit-fires <84981998+Spit-fires@users.noreply.github.com>
2021-07-04 12:18:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Aoijs.Bot({
   shardAmount: 2, //Shard amount 
   mobile: false, //true or false - Discord Mobile Status
   //dbhToken: "API KEY", Remove // if using, get an API Key from their Server
-  token: "ODUyMDUwNzA3MTM3OTUzODIy.YMBMDg.NLmYhPpZF9jDI1FX5YH_zVBRQXg", //Discord Bot Token
+  token: "ODUyMDUwNzA3MTM3OTUzODIy.YMBMDg.7qVss6YaajKP01x0TixduFsRogo", //Discord Bot Token
   prefix: ["!"], //Change PREFIX to your Prefix
   autoUpdate: false, // set to true if version should be updated automatically after a package update
 })
